95ac7337df73010464b455add6af660e0356e887
terzurumluoglu <34763358+terzurumluoglu@users.noreply.github.com>
2020-02-03 20:26:49 +0300

    app.import.ts has added and edited

---
+++ src/app/app.imports.ts
@@ -0,0 +1,7 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+
+export const MODULES = [
+    BrowserModule,
+    AppRoutingModule
+]
